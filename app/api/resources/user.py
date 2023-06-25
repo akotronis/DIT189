66c34480f9aff36f278e9dbc8e9cf8119fd6b226
akotronis <akotronis@gmail.com>
2023-06-25 22:07:10 +0300

    Remove unnecessary staff from resources file

--- app/api/resources/user.py
+++ app/api/resources/user.py
@@ -9,7 +9,7 @@ from ..database import DataBase
 from ..db import db
 from ..models import User
 from ..keycloak import KeycloakAPI
-from ..schemas import UserInputSchema, UserOutputSchema#, UsersOutputSchema
+from ..schemas import UserInputSchema, UserOutputSchema
 
 
 blp = Blueprint('Users', __name__)
@@ -32,97 +32,3 @@ class UserList(MethodView):
         role = query_args.get('role', [])
         users = DataBase.get_users(role=role)
         return users
-
-
-@blp.route('/users/logout')
-class Logout(MethodView):
-    def get(self):
-        issuer_url = None #oidc.client_secrets.get('issuer')
-        hosturl = os.getenv('HOST_MAIN_URL')
-        # oidc.logout()
-        return redirect(f'{issuer_url}/protocol/openid-connect/logout?redirect_uri={hosturl}')
-
-
-@blp.route('/')
-class Dum(MethodView):
-    def get(self):
-        DataBase.get_active_marriages()
-        return {'message': 'GET'}
-    
-    def post(self):
-        # print(g.oidc_token_info['sub'])
-        # print(request.headers)
-        # print(request.form)
-        print('============== 11 =================')
-        print('=============== 22 ================')
-        # print('\n'.join(dir(oidc)))
-        return {'message': 'POST'}
-
-# @blp.route('/users/register')
-# class UserRegister(MethodView):
-#     @blp.arguments(UserSchema)
-#     def post(self, user_data):
-#         user = UserModel(
-#                 username=user_data['username'],
-#                 password=pbkdf2_sha256.hash(user_data['password'])
-#             )
-#         db.session.add(user)
-#         try:
-#             db.session.commit()
-#         except IntegrityError:
-#             db.session.rollback()
-#             abort(400, message='A user with this username already exists')
-#         except:
-#             db.session.rollback()
-#             abort(500, message='Could not register user')
-#         return {'message': 'User created succesfully'}, 201
-
-
-# @blp.route('/users/login')
-# class UserLogin(MethodView):
-#     @blp.arguments(UserSchema)
-#     def post(self, user_data):
-#         user = UserModel.query.filter(UserModel.username == user_data['username']).first()
-#         if user and pbkdf2_sha256.verify(user_data['password'], user.password):
-#             # user.id is stored as value of 'sub' key in JWT payload claims
-#             access_token = create_access_token(identity=user.id, fresh=True)
-#             return {
-#                 'access_token': access_token,
-#             }
-#         abort(401, message='Invalid credentials')
-
-
-# @blp.route('/users/logout')
-# class UserLogout(MethodView):
-#     @jwt_required()
-#     def post(self):
-#         jti = get_jwt()['jti']
-#         BLOCKLIST.add(jti)
-#         return {'message': 'Successfully logged out.'}
-
-
-# @blp.route('/users/<int:user_id>')
-# class User(MethodView):
-
-#     @jwt_required()
-#     @blp.response(200, UserSchema)
-#     def get(self, user_id):
-#         if get_jwt()['sub'] != user_id:
-#             abort(401, message='User missmatched. Could not get user')
-#         user = UserModel.query.get_or_404(user_id)
-#         # If we want to make sure we don't send password to Schema in the first place:
-#         # user = UserModel.query.with_entities(UserModel.username).filter(UserModel.id==user_id).first()
-#         return user
-
-#     @jwt_required()
-#     def delete(self, user_id):
-#         user = UserModel.query.get_or_404(user_id)
-#         if get_jwt()['sub'] != user_id:
-#             abort(401, message='User missmatched. Could not delete user')
-#         try:
-#             db.session.delete(user)
-#             db.session.commit()
-#         except:
-#             db.session.rollback()
-#             abort(500, message='Could not delete user')
-#         return {'message': 'User deleted succesfully'}, 200
