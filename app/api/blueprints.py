1be7119ae7945755587f5b7f662328ddabc6b523
akotronis <akotronis@gmail.com>
2023-06-27 22:12:45 +0300

    Register new endpoints in blueprints

--- app/api/blueprints.py
+++ app/api/blueprints.py
@@ -1,3 +1,5 @@
+from .resources.divorce import blp as DivorceBlueprint
+from .resources.marriage import blp as MarriageBlueprint
 from .resources.user import blp as UserBlueprint
 
 from .resources.rules import blp_rules
@@ -6,5 +8,6 @@ from .resources.rules import blp_rules
 
 def register_blueprints(api):
     api.register_blueprint(blp_rules)
-
+    api.register_blueprint(DivorceBlueprint, url_prefix='/', name='Cases')
+    api.register_blueprint(MarriageBlueprint, url_prefix='/', name='Marriages')
     api.register_blueprint(UserBlueprint, url_prefix='/', name='Users')
