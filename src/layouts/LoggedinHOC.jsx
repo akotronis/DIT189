4567eae887b78554d051e1ec134dff45a77d9078
paanastasiadis <panag1234@outlook.com>
2023-07-15 01:36:00 +0300

    Create HOC that provides logged user to children

---
+++ src/layouts/LoggedinHOC.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useAccessToken } from '../context/Auth';
+import { decodeAccessToken } from '../utils/keycloak_utils';
+import { useEffect } from 'react';
+
+const USERS_URL = 'http://localhost:5000/users?self=1&contains=';
+
+export const LoggedInHOC = ( props ) => {
+  let [user, setUser] = useState([]);
+
+  const { token } = useAccessToken();
+
+  const userEmail = decodeAccessToken(token.accessToken).email;
+
+  // Fetch data when the component mounts
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(USERS_URL + userEmail, {
+          headers: {
+            Authorization: `Bearer ${token.accessToken}`,
+          },
+        }); // Replace with your actual API endpoint
+        const userData = await response.json();
+        setUser(userData[0]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, [token.accessToken, userEmail, setUser]);
+
+  const ClonedChildren = React.cloneElement(props.children,{user});
+
+  return <div className="hoc">{ClonedChildren}</div>};
