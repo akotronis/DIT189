a3a3d0304403b64c0757870fb895a37b294daf4f
paanastasiadis <panag1234@outlook.com>
2023-07-16 02:31:47 +0300

    Add yaml files to deploy on Kubernetes

--- app/api/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# So that we can do from models import ... instead of from models.book import ...
-from .user import User
-from .marriage import Marriage
-from .user_marriage import UsersMarriages
-from .divorce import Divorce
-from .user_divorce import UsersDivorces
--- app/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-python -m api.initialize
-python run.py
